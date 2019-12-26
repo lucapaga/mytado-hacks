6537f6bb73a6ba7eb4dc312925f9671599b4277c
Luca Paganelli <luca.paga@gmail.com>
2019-12-26 14:25:51 +0100

    all queries are done

--- bash/A5.get_data.sh
+++ bash/A5.get_data.sh
@@ -9,7 +9,10 @@ TADO_HOME_ID=$(cat ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_H
 NR_OF_ZONES=$(jq ". | length" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_ZONES_JSON_RESULT})
 zone_idx=0
 
-echo "ZONE_NAME;TEMP_TIMESTAMP;TEMP_VALUE;HUMIDITY_TIMESTAMP;HUMIDITY_VALUE" > ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_CSV}
+CSV_HEADER="ZONE_ID;ZONE_NAME;LINK_TS;LINK_STATE"
+CSV_HEADER="${CSV_HEADER};TEMP_TIMESTAMP;TEMP_VALUE;HUMIDITY_TIMESTAMP;HUMIDITY_VALUE;HEATING_POWER_TIMESTAMP;HEATING_POWER_VALUE"
+CSV_HEADER="${CSV_HEADER};OBJECTIVE_TEMP_VALUE;OBJECTIVE_HEATING_VALUE" 
+echo ${CSV_HEADER} > ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_CSV}
 
 for aZone in $(seq 1 1 ${NR_OF_ZONES})
 do
@@ -21,20 +24,31 @@ do
     let "zone_idx=aZone-1"
     echo "zone idx = ${zone_idx}"
 
-    ZONE_NAME=$(jq ".[${zone_idx}].name" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_ZONES_JSON_RESULT})
-    TEMP_TIME=$(jq ".sensorDataPoints.insideTemperature.timestamp" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json)
-    TEMP_CELSIUS=$(jq ".sensorDataPoints.insideTemperature.celsius" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json)
-    HUMID_TIME=$(jq ".sensorDataPoints.humidity.timestamp" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json)
-    HUMID_PERC=$(jq ".sensorDataPoints.humidity.percentage" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json)
+    CSV_LINE=""
 
-    echo "${ZONE_NAME};${TEMP_TIME};${TEMP_CELSIUS};${HUMID_TIME};${HUMID_PERC}" >> ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_CSV}
+    ZONE_ID=$(jq ".[${zone_idx}].id" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_ZONES_JSON_RESULT} | sed "s/\"//g")
+    ZONE_NAME=$(jq ".[${zone_idx}].name" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_ZONES_JSON_RESULT} | sed "s/\"//g")
+    CSV_LINE="${ZONE_ID};\"${ZONE_NAME}\""
+
+    STATE_TIME=$(date -u +"%FT%T.000Z")
+    STATE_VALUE=$(jq ".link.state" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    CSV_LINE="${CSV_LINE};${STATE_TIME};\"${STATE_VALUE}\""
+
+    TEMP_TIME=$(jq ".sensorDataPoints.insideTemperature.timestamp" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    TEMP_CELSIUS=$(jq ".sensorDataPoints.insideTemperature.celsius" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    CSV_LINE="${CSV_LINE};${TEMP_TIME};${TEMP_CELSIUS}"
+
+    HUMID_TIME=$(jq ".sensorDataPoints.humidity.timestamp" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    HUMID_PERC=$(jq ".sensorDataPoints.humidity.percentage" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    CSV_LINE="${CSV_LINE};${HUMID_TIME};${HUMID_PERC}"
+
+    HEAT_PWR_TIME=$(jq ".activityDataPoints.heatingPower.timestamp" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    HEAT_PWR_VALUE=$(jq ".activityDataPoints.heatingPower.percentage" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    CSV_LINE="${CSV_LINE};${HEAT_PWR_TIME};${HEAT_PWR_VALUE}"
+
+    OBJ_TEMP_VALUE=$(jq ".setting.temperature.celsius" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    OBJ_HEAT_VALUE=$(jq ".setting.power" ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PERZONE_JSON_RESULT_PREFIX}_${aZone}.json | sed "s/\"//g")
+    CSV_LINE="${CSV_LINE};${OBJ_TEMP_VALUE};\"${OBJ_HEAT_VALUE}\""
+
+    echo "${CSV_LINE}" >> ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_CSV}
 done
-
-exit 0
-
-echo "invoking uri >${TADO_API_HOME_DETAILS_BASE_URI}/${TADO_HOME_ID}/weather<"
-curl "${TADO_API_HOME_DETAILS_BASE_URI}/${TADO_HOME_ID}/weather" \
-     -H "Authorization: Bearer ${BEARER_TOKEN}" \
-     -o ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_WEATHER_JSON_RESULT}
-
-#jq .homeId ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_API_PROFILE_JSON_RESULT} > ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_HOME_ID}
