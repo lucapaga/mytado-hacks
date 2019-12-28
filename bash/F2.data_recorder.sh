091773cfe7760cf4fdeec30960deea8e4aa242e2
Luca Paganelli <luca.paga@gmail.com>
2019-12-29 00:34:09 +0100

    now available: recorder

---
+++ bash/F2.data_recorder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. ./00.variables.sh
+
+./01.create_session.sh
+./02.login.sh
+
+#BEARER_TOKEN=$(cat ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${OAUTH_ACCESS_TOKEN})
+
+./A1.profile.sh
+./A2.home_details.sh
+
+#TADO_HOME_ID=$(cat ${WORKDIR}/${SESSION_DIR_PREFIX}${SESSION_NAME}/${TADO_DATA_HOME_ID})
+
+./A3.zones.sh
+./A4.weather.sh
+./A5.get_data.sh #${TH_ZONE} ${ROOM1_ZONE}
--- bash/M1.intelligen_tuner_cron.sh
+++ bash/M1.intelligen_tuner_cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Example crontab
+# */5 9-21 * * * cd /home/luca_paganelli/mytado-hacks/bash && ./M1.intelligen_tuner_cron.sh
+
 . ./00.variables.sh
 
 ./F1.intelligent_tuner.sh 5 2
