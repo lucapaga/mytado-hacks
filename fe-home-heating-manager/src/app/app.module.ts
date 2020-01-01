e8eb50d9337d9ab917c3f4a5e29b596e95ed615c
Luca Paganelli <luca.paga@gmail.com>
2020-01-01 16:57:42 +0100

    Removing prepacked pages

--- fe-home-heating-manager/src/app/app.module.ts
+++ fe-home-heating-manager/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
+import { MainComponent } from './app/base/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+//    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
