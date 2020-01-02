304db54f3093c8ab68c2786125c3e5a8f05743e1
Luca Paganelli <luca.paga@gmail.com>
2020-01-02 23:57:54 +0100

    miglioramento grafico

--- fe-home-heating-manager/README.md
+++ fe-home-heating-manager/README.md
@@ -2,6 +2,14 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.19.
 
+Additionally a Docker recipe is created.
+
+Build with
+`docker build -t lucapaga/hhm-fe:0.0.1 .`
+
+Run
+`docker run --name fe-home-heating-mgr -d -p 8080:80 --restart always lucapaga/hhm-fe:0.0.1`
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- fe-home-heating-manager/src/app/scvs/special-schedule-manager.service.ts
+++ fe-home-heating-manager/src/app/scvs/special-schedule-manager.service.ts
@@ -12,32 +12,7 @@ export class SpecialScheduleManagerService {
   constructor(private http: HttpClient) { }
 
   listSpecialSchedulesFor(homeId: number): Observable<SpecialSchedule[]> {
-    return this.http.get<SpecialSchedule[]>("/mthapis/homes/" + homeId + "/special-schedules/");
-    /*
-    this.http.post(
-        "https://auth.tado.com/oauth/token", 
-        {}, 
-        {
-          headers: { 'Content-Type': 'application/json' }, 
-          params: {
-            client_id: "tado-web-app",
-            client_secret: "wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc",
-            grant_type: "password",
-            password: password,
-            scope: "home.user",
-            username: user
-          }
-        }).subscribe(
-          (data) => {
-            var bt : string = data["access_token"];
-            this.http.delete<SpecialSchedule[]>(
-              "https://my.tado.com/api/v2/homes/" + homeId + "/zones/" + zoneId + "/overlay", 
-              {
-                headers: { 'Content-Type': 'application/json', Authorization: "Bearer " + bt }
-              });
-          }
-        );
-    */
+    return this.http.get<SpecialSchedule[]>(environment.mthapisBaseUri + "/homes/" + homeId + "/special-schedules/");
     /*
     return of([
       new SpecialSchedule("ID1", "Primo SRV", false),
@@ -49,14 +24,14 @@ export class SpecialScheduleManagerService {
 
   activateSchedule(homeId: number, specialSchedule: SpecialSchedule): Observable<Object> {
     return this.http.put<SpecialSchedule>(
-              "/mthapis/homes/" + homeId + "/special-schedules/" + specialSchedule.id + "/ON", 
+              environment.mthapisBaseUri + "/homes/" + homeId + "/special-schedules/" + specialSchedule.id + "/ON", 
               {}
             );
   }
 
   deactivateSchedule(homeId: number, specialSchedule: SpecialSchedule): Observable<Object> {
     return this.http.put(
-              "/mthapis/homes/" + homeId + "/special-schedules/" + specialSchedule.id + "/OFF", 
+              environment.mthapisBaseUri + "/homes/" + homeId + "/special-schedules/" + specialSchedule.id + "/OFF", 
               {}
             );
   }
