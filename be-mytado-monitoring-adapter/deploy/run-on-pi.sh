141c29c66b5d31428136883fc2f2e90dcec136c5
Luca Paganelli <luca.paga@gmail.com>
2020-01-09 00:11:27 +0100

    still having path errors

--- be-mytado-monitoring-adapter/deploy/run-on-pi.sh
+++ be-mytado-monitoring-adapter/deploy/run-on-pi.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd ..
-cd ../be-commons
+cd ../../be-commons
 npm i && npm run build
-cd ../business-logic
+cd ../be-mytado-monitoring-adapter/business-logic
 npm i && npm run build
 cd ../standalone
 npm i && npm run start-dev
