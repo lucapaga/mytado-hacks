0a6e15f371a216af1579e6bcdcd880e5200d556f
Luca Paganelli <luca.paga@gmail.com>
2020-01-25 17:36:20 +0100

    now google cloud function!

--- be-mytado-monitoring-adapter/gcp-functions/gcp_cf_deploy.bat
+++ be-mytado-monitoring-adapter/gcp-functions/gcp_cf_deploy.bat
@@ -1,3 +1,2 @@
-rmdir /S /Q node_modules
-npm i
-gcloud functions deploy mth-monitoring --set-env-vars MYTADO_SA_USER=luca.paga@gmail.com,MYTADO_SA_PWD=4G@LdQA3vL75Bmx --trigger-http --runtime nodejs10 --allow-unauthenticated --region europe-west1
+
+gcloud functions deploy mthMonitoring --set-env-vars MYTADO_SA_USER=luca.paga@gmail.com,MYTADO_SA_PWD=4G@LdQA3vL75Bmx --trigger-http --runtime nodejs10 --allow-unauthenticated --region europe-west1
--- be-mytado-monitoring-adapter/gcp-functions/index.js
+++ be-mytado-monitoring-adapter/gcp-functions/index.js
@@ -5,38 +5,85 @@ const { pathToRegexp, match, parse, compile } = require("path-to-regexp");
 
 const { MyTadoMonitoringServices, MyTadoServicesZonesAdapter } = require('mytado-hacks-be-monitoring-adapter-bl');
 
-exports.helloworld = (req, res) => {
-  res.send('Hello World!');
-};
 
-exports.getZonesInformation = (req, res) => {
-  var keys = [];
-  var re = pathToRegexp('/homes/:homeId/zones', keys, {strict: false});
-  var pathVars = re.exec(req.path);
-  if (pathVars) {
+
+exports.mthMonitoring = (req, res) => {
+  const matchZones = match("/homes/:homeId/zones", { decode: decodeURIComponent });
+  const matchZonesOnPath = matchZones(req.path);
+  console.log("MatchZones", matchZonesOnPath);
+
+  const matchZoneDetails = match("/homes/:homeId/zone/:zoneId", { decode: decodeURIComponent });
+  const matchZoneDetailsOnPath = matchZoneDetails(req.path);
+  console.log("MatchZoneDetails", matchZoneDetailsOnPath);
+
+  if (matchZonesOnPath) {
+    console.log("Going with zones");
+    var keys = [];
+    var re = pathToRegexp('/homes/:homeId/zones', keys, { strict: false });
+    var pathVars = re.exec(req.path);
+    //console.log("This is the compiled regex result", pathVars);
+    if (pathVars) {
       console.log(JSON.stringify(pathVars));
       var homeId = pathVars[1];
       console.log("Working on HOME ", homeId);
 
       try {
-        console.log("Request Parameters: ", req.params);
+        //console.log("Request Parameters: ", req.params);
         const applicationService = new MyTadoMonitoringServices();
         applicationService.getZonesIn(+homeId).then(
-            (data) => { 
-                const mytsa = new MyTadoServicesZonesAdapter();
-                return res.status(OK).json(mytsa.deserializeZones(data)); 
-            }, 
-            (err) => {
-            return res.status(BAD_REQUEST).json({
-                error: err.message,
-            });
-        });
-      } catch (err) {
-          logger.error(err.message, err);
-          return res.status(BAD_REQUEST).json({
+          (data) => {
+            //console.log("Zones retrieved: ", data);
+            const mytsa = new MyTadoServicesZonesAdapter();
+            console.log("Returning ...");
+            return res.status(200).json(mytsa.deserializeZones(data));
+          },
+          (err) => {
+            return res.status(400).json({
               error: err.message,
+            });
           });
+      } catch (err) {
+        logger.error(err.message, err);
+        return res.status(400).json({
+          error: err.message,
+        });
       }
+    }
+  } else if(matchZoneDetailsOnPath) {
+    console.log("Going with zone details");
+    var keys = [];
+    var re = pathToRegexp('/homes/:homeId/zone/:zoneId', keys, { strict: false });
+    var pathVars = re.exec(req.path);
+    //console.log("This is the compiled regex result", pathVars);
+    if (pathVars) {
+      //console.log(JSON.stringify(pathVars));
+      var homeId = pathVars[1];
+      var zoneId = pathVars[2];
+      console.log("Working on HOME " + homeId + ", ZONE " + zoneId);
+
+      try {
+        //console.log("Request Parameters: ", req.params);
+        const applicationService = new MyTadoMonitoringServices();
+        applicationService.getZoneTelemetricsAndConfiguration(+homeId, +zoneId).then(
+          (data) => {
+            console.log("Returning ...");
+            return res.status(200).json(data); 
+          },
+          (err) => {
+            return res.status(400).json({
+              error: err.message,
+            });
+          });
+      } catch (err) {
+        logger.error(err.message, err);
+        return res.status(400).json({
+          error: err.message,
+        });
+      }
+    }
+  } else {
+    console.log("It's not a zones query");
+    res.status(404).send("Unsupported operation");
   }
-  //res.send('Hello World!');
+
 };
